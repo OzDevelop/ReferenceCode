a62b509596a99ff1bc81dc190a6afaa3899b309f
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-07-14 20:54:33 +0900

    Date+Extention 파일 추가

---
+++ Reference_SwiftUI/Reference_SwiftUI/Core/Extension/Date+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Extension.swift
+//  Reference_SwiftUI
+//
+//  Created by 변상필 on 7/14/24.
+//
+
+import Foundation
---
+++ Reference_SwiftUI/Reference_SwiftUI/Reference_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reference_SwiftUIApp.swift
+//  Reference_SwiftUI
+//
+//  Created by 변상필 on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Reference_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
