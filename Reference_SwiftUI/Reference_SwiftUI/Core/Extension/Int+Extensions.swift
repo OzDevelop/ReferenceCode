e29b4aca166b32a3efddafef04e26ca421d0ac6c
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-10-13 22:09:39 +0900

    Feat: Int+Extensions 추가
    숫자 천 단위마다 문자열에 콤마(,) 추가하기

---
+++ Reference_SwiftUI/Reference_SwiftUI/Core/Extension/Int+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Int+Extensions.swift
+//  Reference_SwiftUI
+//
+//  Created by 변상필 on 10/13/24.
+//
+
+import Foundation
+
+extension Int {
+    //MARK: - 숫자 천 단위마다 문자열에 콤마(,) 추가하기
+  public func toStringWithComma() -> String {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .decimal
+    
+      
+      return numberFormatter.string(from: NSNumber(value: self)) ?? "0"
+
+  }
+}
