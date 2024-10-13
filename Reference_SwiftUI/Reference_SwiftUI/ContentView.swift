974b5be715cb23b343dc7c183794d9b55ae01495
byunsp3610@gmail.com <byunsp3610@gmail.com>
2024-10-13 21:20:41 +0900

    Feat: 기본 Date+Extentions 추가

--- Reference_SwiftUI/Reference_SwiftUI/ContentView.swift
+++ Reference_SwiftUI/Reference_SwiftUI/ContentView.swift
@@ -10,10 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                print(Date.now.basic)
+            } label: {
+                Text("Date")
+                    
+            }
+            .buttonStyle(.bordered)
+            
+//            Button {
+//                print(yesterday)
+//            } label: {
+//                Text("yesterday Date")
+//            }
+//            .buttonStyle(.bordered)
         }
         .padding()
     }
--- Reference_SwiftUI/Reference_SwiftUI/Core/Extension/Date+Extensions.swift
+++ Reference_SwiftUI/Reference_SwiftUI/Core/Extension/Date+Extensions.swift
@@ -1,8 +1,71 @@
 //
-//  Date+Extensions.swift
-//  Reference_SwiftUI
+// Swift에서 기본으로 제공하는 날짜를 다루기 위한 Date 구조체 타입
 //
-//  Created by 변상필 on 7/14/24.
-//
-
 import Foundation
+
+extension Date {
+  // MARK: - 기본 & 짧은 날짜 표시
+  public var basic: String {
+    return toString("yyyy년 M월 d일")
+  }
+  public var summary: String {
+    return toString("yyyy-MM-dd")
+  }
+  
+  // MARK: - 요일 포함 날짜 표시
+  public var basicWithDay: String {
+    return toString("yyyy년 M월 d일 (EEEEE)")
+  }
+  
+  // MARK: - 시간 표시
+  public var timeWithoutSecond: String {
+    return toString("a h시 m분")
+  }
+  public var timeWithSecond: String {
+    return toString("a h시 m분 s초")
+  }
+  public var shortTimeWithoutSecond: String {
+    return toString("HH:mm")
+  }
+  public var shortTimeWithSecond: String {
+    return toString("HH:mm:ss")
+  }
+  
+  // MARK: - 시간 포함 짧은 날짜 표시
+  public var summaryWithTimeWithoutSecond: String {
+    return toString("yyyy-MM-dd HH:mm")
+  }
+  public var summaryWithTimeWithSecond: String {
+    return toString("yyyy-MM-dd HH:mm:ss")
+  }
+  
+  // MARK: - 요일 & 시간 포함 기본 날짜 표시
+  public var basicWithDayAndTimeWithoutSecond: String {
+    return toString("yyyy년 M월 d일 (EEEEE) a h시 m분")
+  }
+  public var basicWithDayAndTimeWithSecond: String {
+    return toString("yyyy년 M월 d일 (EEEEE) a h시 m분 s초")
+  }
+  
+  // MARK: - Date -> String
+  public func toString(_ dateFormat: String) -> String {
+    return DateFormatter
+      .convertToKoKR(dateFormat: dateFormat)
+      .string(from: self)
+  }
+}
+
+extension DateFormatter {
+  public static func convertToKoKR(dateFormat: String) -> DateFormatter {
+    let dateFormatter = createKoKRFormatter()
+    dateFormatter.dateFormat = dateFormat
+    return dateFormatter
+  }
+}
+
+private func createKoKRFormatter() -> DateFormatter {
+  let dateFormatter = DateFormatter()
+  dateFormatter.locale = Locale(identifier: "ko_KR")
+  dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+  return dateFormatter
+}
